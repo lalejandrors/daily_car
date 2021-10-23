89176b7c6dc1f8a4be1aa30cd634e9c3c39c7410
lalejandrors <lalejandrors@gmail.com>
2021-10-23 17:09:12 -0500

    base antes options o composition api

---
+++ src/modules/daybook/components/Fab.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="btn btn-primary">
+      <!-- puedo usar dos class en este caso cuando el otro lo uso con ":", lo que ha a hacer es fusionar lo de ambos en uno -->
+      <i class="fa fa-2x" :class="icon"></i>
+  </button>
+</template>
+
+<script>
+export default {
+    props: {
+        icon: {
+            type: String,
+            default: 'fa-plus'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    button{
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        width: 60px;
+        height: 60px;
+        border-radius: 100%;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home'
+
+import daybookRouter from '../modules/daybook/router'
 
 const routes = [
   {
@@ -13,7 +15,11 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About')
+  },
+  {
+    path: '/daybook',
+    ...daybookRouter//esparcimos lo que definimos en el index del router del modulo en cuestión
   }
 ]
 
